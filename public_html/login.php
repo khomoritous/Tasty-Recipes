a8d94a3b2a770c14ce9fe1bb751a7ae9da615ef3
Monde Mampe <monde.mampe@gmail.com>
2017-11-24 03:59:50 +0100

    har testat med en enkel testfil för att kolla om värdena från loginformen sätts i globala get-arrayen.

--- public_html/login.php
+++ public_html/login.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+
+/*början på en session för att kunna skapa tillstånd mellan klient och server*/
+session_start();
+
+/*säkerhet, för att kunna identifiera rätt form så att en användare inte använder sin egen
+och att det bara finns en unik form så att man inte skickar flera former till databasen.*/
+$form_token = md5(uniqid('auth', true) );
+
+/*sätta form token i session arrayen*/
+$_SESSION['form_token'] = $form_token;
+?>
 
 
 <html>
@@ -32,26 +38,21 @@ and open the template in the editor.
                         <a class="menulink" href="pannkakor.html">Pannkakor</a><span class="hide"> | </span>
                         <a class="menulink" href="köttbullar.html">Köttbullar</a><span class="hide"> | </span>
                         <a class="menulink" href="kalender.html">Kalender</a><span class="hide"> | </span>
-                        <a class="menulink active" href="login.html">Login</a><span class="hide"> | </span>
+                        <a class="menulink active" href="login.php">Login</a><span class="hide"> | </span>
                         </p>
                 </div>
         
              <div id="content">
                  
-                <form action="test.php" method='GET'>
+                 <form action="valideraanvändardata.php" method='get'>
                     <fieldset><legend>Login:</legend>
                           <label>Användarnamn:</label>
                           <input type="text" placeholder="Enter Username" name="tasty_username" required maxlength="20" /> <br />
 
                           <label>Lösenord:</label>
                           <input type="password" placeholder="Enter Password" name="tasty_password" required maxlength="40" />
-                          <!--<input type="hidden" name="form_token" value="<?php echo $form_token; />-->
-                          <input type="submit" value="Logga in" />
-                          <!--
-                          <input type="checkbox" checked="checked"> Remember me
-
-                          <button type="button" class="cancelbtn">Cancel</button>
-                          <span class="psw">Forgot <a href="#">password?</a></span> -->
+                          <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+                          <input type="submit" value="Logga in" />                          
                     </fieldset>
                 </form> 
              </div>
@@ -66,3 +67,4 @@ and open the template in the editor.
     
     </body>
 </html>
+
